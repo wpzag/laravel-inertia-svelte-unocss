29444a5d466b924548025542b21e1543645babc6
wpzag <egypt.zag.eg@gmail.com>
2023-01-09 22:37:32 +0200

    add pest

--- tests/Unit/ExampleTest.php
+++ tests/Unit/ExampleTest.php
@@ -1,18 +1,6 @@
 <?php
 
-namespace Tests\Unit;
-
-use PHPUnit\Framework\TestCase;
-
-class ExampleTest extends TestCase
-{
-    /**
-     * A basic test example.
-     *
-     * @return void
-     */
-    public function test_that_true_is_true()
-    {
-        $this->assertTrue(true);
-    }
-}
+    it('should return a successful response', function () {
+        $response = $this->get('/');
+        $response->assertStatus(200);
+    });
